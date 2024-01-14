34f3b84c981b99193ef2b673145498c636700f49
furaizi <furachannel16@gmail.com>
2024-01-14 17:44:02 +0200

    Rewrite forEach to for..of loop

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,12 +5,13 @@
 let getSum = (obj) => {
   let sum = 0;
   let keys = Object.keys(obj);
+  let value;
 
-  keys.forEach((key) => {
-    [];
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
+  for (const key of keys) {
+    value = obj[key];
+    if (typeof value === 'number')
+      sum += value;
+  }
 
   return sum;
 };
