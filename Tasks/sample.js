8cdfb386333dd073c4ead27d21c520c15b1951ca
furaizi <furachannel16@gmail.com>
2024-01-14 20:11:09 +0200

    Rename a function

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -2,9 +2,9 @@
 
 'use strict';
 
-let SAMPLE = (arr) => {
+let getRandomElement = (arr) => {
   arr = arr[Math.floor(Math.random() * arr.length)];
   return arr;
 };
 
-module.exports = SAMPLE;
+module.exports = getRandomElement;
