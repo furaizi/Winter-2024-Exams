0c27bd8b69a80567a823c7c78f02ba92fd58c6e8
furaizi <furachannel16@gmail.com>
2024-01-14 17:35:00 +0200

    Add spacing and rename a function

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,15 +2,17 @@
 
 'use strict';
 
-let count = (obj) => {
+let getSum = (obj) => {
   let sum = 0;
   let keys = Object.keys(obj);
+
   keys.forEach((key) => {
     [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
+
   return sum;
 };
 
-module.exports = count;
+module.exports = getSum;
