5b128635f460ce5bef9a0abbe569892516f383bc
furaizi <furachannel16@gmail.com>
2024-01-14 17:53:49 +0200

    Add 'use strict'

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,9 +1,11 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
+'use strict';
+
+let duplicate = (value, N) => {
   if (N <= 0) return [];
   else {
-    res = [];
+    let res = [];
     for (let i = 0; i < N; i++) {
       res[i] = value;
     }
