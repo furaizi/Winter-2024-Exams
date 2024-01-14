14c0d784d6b2c8c28a435222334c5c778c229def
furaizi <furachannel16@gmail.com>
2024-01-14 14:18:40 +0200

    Add 'let' before variables initializations

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,10 +3,10 @@
 'use strict';
 
 getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p);
+  let i = str.indexOf(p);
   if (i === -1) return '';
   else {
-    k = i + p.length;
+    let k = i + p.length;
     str = str.substring(k);
     if (s) {
       i = str.indexOf(s);
