cf191cb2f3d276b48c909771198066d14f26d894
furaizi <furachannel16@gmail.com>
2024-01-14 17:00:26 +0200

    Add spacing and swap return condition

--- Tasks/range.js
+++ Tasks/range.js
@@ -4,15 +4,17 @@
 
 let getRange = (...range) => {
   let [from, to] = range;
-  if (to >= from) {
-    range = new Array(to - from + 1);
-    for (let i = from; i <= to; i++) {
-      range[i - from] = i;
-    }
-  } else {
+
+  if (to < from)
     return [];
+
+  range = new Array(to - from + 1);
+  for (let i = from; i <= to; i++) {
+    range[i - from] = i;
   }
+  
   return range;
+
 };
 
 module.exports = getRange;
