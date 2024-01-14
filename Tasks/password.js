6b4bac88eb92305c928f56bcceed2ac96ab3da23
furaizi <furachannel16@gmail.com>
2024-01-14 16:06:44 +0200

    Remove unnecessary variable

--- Tasks/password.js
+++ Tasks/password.js
@@ -3,10 +3,9 @@
 'use strict';
 
 let generatePassword = (alphabet, passwordLength) => {
-  const MAX = alphabet.length;
   let key = '';
   for (let i = 0; i < passwordLength; i++) {
-    let index = Math.floor(Math.random() * MAX);
+    let index = Math.floor(Math.random() * alphabet.length);
     key = key + alphabet[index];
   }
   return key;
