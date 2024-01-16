dbfd6721a87cd9d5f647be1d98aa602adc4227cb
furaizi <furachannel16@gmail.com>
2024-01-16 19:39:51 +0200

    Change 'let' variables to 'const' ones

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,17 +2,17 @@
 
 'use strict';
 
-let getValueBetween = (str, prefix, suffix) => {
-  let prefixStart = str.indexOf(prefix);
-  let suffixStart = str.indexOf(suffix);
+const getValueBetween = (str, prefix, suffix) => {
+  const prefixStart = str.indexOf(prefix);
+  const suffixStart = str.indexOf(suffix);
 
   if (prefixStart === -1)
     return '';
   if (suffixStart === -1)
     return '';
 
-  let prefixEnd = prefixStart + prefix.length;
-  let substring = str.substring(prefixEnd, suffixStart);
+  const prefixEnd = prefixStart + prefix.length;
+  const substring = str.substring(prefixEnd, suffixStart);
 
   return substring;
 };
